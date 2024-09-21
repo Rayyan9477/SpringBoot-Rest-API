dcb119272b9d297a236badccb7b776d58f84cd23
Rayyan9477 <rayyanahmed265@yahoo.com>
2024-09-21 23:54:29 +0500

    Initail

---
+++ SpringBootRest/src/test/java/Rayyan/SpringBootRest/SpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package Rayyan.SpringBootRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
